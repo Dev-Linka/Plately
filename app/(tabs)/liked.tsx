6ecb05d54ffa862e1ede2aada91f31ef62fa6233
SePuede <stale4669@gmail.com>
2025-07-11 21:07:01 +0200

    initial commit

---
+++ app/(tabs)/liked.tsx
@@ -0,0 +1,18 @@
+import { Image } from 'expo-image';
+import { Platform, StyleSheet, Text, View } from 'react-native';
+
+import { Collapsible } from '@/components/Collapsible';
+import { ExternalLink } from '@/components/ExternalLink';
+import ParallaxScrollView from '@/components/ParallaxScrollView';
+import { ThemedText } from '@/components/ThemedText';
+import { ThemedView } from '@/components/ThemedView';
+import { IconSymbol } from '@/components/ui/IconSymbol';
+
+export default function TabTwoScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Liked</Text>
+    </View>
+  );
+}
+
