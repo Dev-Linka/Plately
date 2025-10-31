fa8697146118cf7d05e922d5f0cc3b7cd2941f9e
SePuede <stale4669@gmail.com>
2025-10-31 10:32:49 +0100

    repaired signup and usernames

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -69,6 +69,11 @@ const AuthForm = ({ onLoginSuccess }: Props) => {
         } = await supabase.auth.signUp({
           email: email,
           password: password,
+          options: {
+            data:{
+              username: username,
+            },
+          },
         })
     
         if (error) Alert.alert(error.message)
